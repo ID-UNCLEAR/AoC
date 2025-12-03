7361fe03995b90fb44dee6aadd5c6f141052af33
Ruben <ruben@rubenvanzanten.nl>
2025-12-03 20:39:28 +0100

    Day 1 part 1

---
+++ Day1/Console/Program.cs
@@ -0,0 +1,29 @@
+﻿var dialIndex = 50;
+var numberOfZeros = 0;
+
+foreach (var line in File.ReadLines("input.txt"))
+{
+    var direction = line[0];
+    var distance = int.Parse(line[1..]);
+
+    if (direction == 'L')
+    {
+        dialIndex = (dialIndex - distance) % 100;
+
+        if (dialIndex < 0)
+        {
+            dialIndex += 100;
+        }
+    }
+    else
+    {
+        dialIndex = (dialIndex + distance) % 100;
+    }
+
+    if (dialIndex == 0)
+    {
+        numberOfZeros++;
+    }
+}
+
+Console.WriteLine(numberOfZeros);
