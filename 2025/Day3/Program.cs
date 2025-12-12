ca1c6717eecad20828d9b53b3cfffc3a3b9d456f
Ruben <ruben@rubenvanzanten.nl>
2025-12-12 13:40:05 +0100

    Day 3 part 1

---
+++ 2025/Day3/Program.cs
@@ -0,0 +1,36 @@
+﻿var totalJoltageOutput = 0;
+
+foreach (var bank in File.ReadLines("input.txt"))
+{
+    var joltageOutputOfBank = 0;
+
+    var joltageRatings = bank.Select(n => int.Parse(new ReadOnlySpan<char>(in n))).ToArray();
+
+    for (var index = 1; index <= 9; index++)
+    {
+        if (!joltageRatings.Contains(index))
+        {
+            continue;
+        }
+
+        var highestJoltageRatingIndex = joltageRatings.IndexOf(index) + 1; // To skip the highest number being included twice
+        var remainingJoltageRatings = joltageRatings[highestJoltageRatingIndex..];
+        if (remainingJoltageRatings.Length == 0)
+        {
+            continue;
+        }
+
+        var secondHighestJoltageRating = remainingJoltageRatings.Max();
+
+        var projectedOutputOfBank = int.Parse($"{index}{secondHighestJoltageRating}");
+
+        if (projectedOutputOfBank > joltageOutputOfBank)
+        {
+            joltageOutputOfBank = projectedOutputOfBank;
+        }
+    }
+
+    totalJoltageOutput += joltageOutputOfBank;
+}
+
+Console.WriteLine($"Total output joltage is {totalJoltageOutput}");
