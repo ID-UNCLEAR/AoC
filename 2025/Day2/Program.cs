2af1e624be2a40f6f8203ee01e9fecf08debf2bf
Ruben <ruben@rubenvanzanten.nl>
2025-12-12 11:35:24 +0100

    Day 2 part 1

--- 2025/Day2/Program.cs
+++ 2025/Day2/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var fileContent = await File.ReadAllTextAsync("input.txt");
+var ranges = fileContent.Split(',');
+var sumOfInvalidIds = 0L;
+
+foreach (var range in ranges)
+{
+    var idRange = range.Split('-');
+    var firstId = long.Parse(idRange[0]);
+    var lastId = long.Parse(idRange[1]);
+
+    for (var currentId = firstId; lastId >= currentId; currentId++)
+    {
+        var currentIdString = currentId.ToString();
+        var halfSize = currentIdString.Length / 2;
+
+        var firstHalf = currentIdString[..halfSize];
+        var secondHalf = currentIdString[halfSize..];
+
+        if (firstHalf == secondHalf)
+        {
+            sumOfInvalidIds += currentId;
+        }
+    }
+}
+
+Console.WriteLine($"The sum of invalid ids is: {sumOfInvalidIds}");
